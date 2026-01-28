5819004c923f393d6baf2ab3b54053a0273beb7c
Oleg <arefevpro@yandex.ru>
2026-01-28 22:23:52 +0300

    delete

--- README.md
+++
@@ -1,2 +0,0 @@
-# arefevpro
-new proj
--- prisma/migrations/0001_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Shader" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "code" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "Shader_pkey" PRIMARY KEY ("id")
-);
